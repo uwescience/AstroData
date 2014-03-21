f46b9ff9076466339f0aa91aaff45fa194ca4c61
Jake Vanderplas <vanderplas@astro.washington.edu>
2014-03-21 15:17:06 -0700

    update site

--- media/js/main.js
+++ media/js/main.js
@@ -1,6 +1,5 @@
-/* This is a jquery function that makes every internal link in the page turn
-   into a smooth, dynamic scroll */
 $(function() {
+  /* Make every internal link in the page turn into a smooth scroll */
   $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
@@ -13,4 +12,19 @@ $(function() {
       }
     }
   });
+
+  /* highlight the appropriate menu button when scrolling */
+  window.onload = window.onscroll = function(){
+    $('.button').select('a[href*=#]:not([href=#])').each(function(i, b){
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      var parent = target.parent();
+      var yref = window.pageYOffset;
+      if(parent.offset()){
+        $(b).toggleClass("active",
+	                 (yref >= parent.offset().top - 100 &&
+                          yref < parent.offset().top + parent.height()));
+      }
+    });
+  }
 });
