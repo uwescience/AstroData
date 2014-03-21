6eb6fca8ad74a83bb2ac09468b2eaeb2efbac7de
Jake Vanderplas <vanderplas@astro.washington.edu>
2014-03-21 14:22:44 -0700

    add some fancy jquery scrolling

---
+++ website/content/media/js/main.js
@@ -0,0 +1,16 @@
+/* This is a jquery function that makes every internal link in the page turn
+   into a smooth, dynamic scroll */
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top - 100
+        }, 500);
+        return false;
+      }
+    }
+  });
+});
